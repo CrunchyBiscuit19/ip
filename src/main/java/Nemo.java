31e74b5d8d569ea3425370472ff1d8574f50e78c
Sim Yue Yang <yueyangsim02@gamil.com>
2026-01-14 16:36:42 +0800

    Rename the project from "Duke" to "Nemo"
    Modify all references to Duke to be about Nemo

---
+++ src/main/java/Nemo.java
@@ -0,0 +1,11 @@
+public class Nemo {
+    public static void main(String[] args) {
+        String logo = " _______                         \n"
+                + " \\      \\   ____   _____   ____  \n"
+                + " /   |   \\_/ __ \\ /     \\ /  _ \\ \n"
+                + "/    |    \\  ___/|  Y Y  (  <_> )\n"
+                + "\\____|__  /\\___  >__|_|  /\\____/ \n"
+                + "        \\/     \\/      \\/        \n";
+        System.out.println("Hello from\n" + logo);
+    }
+}
