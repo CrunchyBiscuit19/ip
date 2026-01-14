e6551ddf78551dd6aabb1f376875256e8a144b2f
Sim Yue Yang <yueyangsim02@gamil.com>
2026-01-14 20:27:13 +0800

    Add task class, and modify the store to hold tasks instead of plain strings

--- src/main/java/Store.java
+++ src/main/java/Store.java
@@ -2,21 +2,27 @@ import java.text.MessageFormat;
 import java.util.ArrayList;
 
 public class Store {
-    private ArrayList<String> store;
-    
-    Store () {
+    private ArrayList<Task> store;
+
+    Store() {
         this.store = new ArrayList<>();
     }
 
-    void add(String item) {
-        this.store.add(item);
+    Task get(int id) {
+        return this.store.get(id);
+    }
+
+    void add(String itemDescription) {
+        this.store.add(new Task(itemDescription));
     }
 
     String generateList() {
-        if (this.store.isEmpty()) return "Nothing added.";
+        if (this.store.isEmpty())
+            return "Nothing added.";
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < store.size(); i++) {
-            sb.append(MessageFormat.format("{0}. {1}\n", String.format("%03d", i + 1), store.get(i)));
+            sb.append(MessageFormat.format("{0}.[{1}] {2}\n", String.format("%03d", i + 1),
+                    store.get(i).getStatusIcon(), store.get(i).description));
         }
         return sb.toString().trim();
     }
---
+++ src/main/java/Task.java
@@ -0,0 +1,21 @@
+public class Task {
+    protected String description;
+    protected boolean done;
+
+    public Task(String description) {
+        this.description = description;
+        this.done = false;
+    }
+
+    public String getStatusIcon() {
+        return (done ? "X" : " "); // mark done task with X
+    }
+
+    public void mark() {
+        done = true;
+    }
+
+    public void unmark() {
+        done = false;
+    }
+}
