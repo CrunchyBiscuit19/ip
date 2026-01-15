90ad33fda1c1ff13b1fe738745ee42062f5c724d
Sim Yue Yang <yueyangsim02@gamil.com>
2026-01-15 20:26:20 +0800

    Dummy commit for A-Enums increment

--- src/main/java/Nemo.java
+++ src/main/java/Nemo.java
@@ -26,7 +26,7 @@ public class Nemo {
             } catch (Exception e) {
                 System.out.println(e.getMessage());
             }
-
+ 
             System.out.println("____________________________________________________________");
         }
 
