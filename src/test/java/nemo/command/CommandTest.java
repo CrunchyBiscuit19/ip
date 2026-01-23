2a4e50df05d2747732b162078e5f5686fd092551
Sim Yue Yang <yueyangsim02@gamil.com>
2026-01-23 21:42:53 +0800

    Add JUnit tests

---
+++ src/test/java/nemo/command/CommandTest.java
@@ -0,0 +1,25 @@
+package nemo.command;  
+
+import nemo.exception.NotCommandException;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CommandTest {
+    @Test
+    public void fromStringTest(){
+        try {
+            assertEquals(Command.fromString("bye"), Command.BYE);   
+            assertEquals(Command.fromString("list"), Command.LIST);   
+            assertEquals(Command.fromString("mark"), Command.MARK);   
+            assertEquals(Command.fromString("unmark"), Command.UNMARK);   
+            assertEquals(Command.fromString("todo"), Command.TODO);   
+            assertEquals(Command.fromString("deadline"), Command.DEADLINE);   
+            assertEquals(Command.fromString("event"), Command.EVENT);   
+            assertEquals(Command.fromString("delete"), Command.DELETE);
+            Command.fromString("wrong");   
+        } catch (NotCommandException e) {
+            assertEquals(e.getMessage(), "Not valid command.");
+        }
+    }
+}
---
+++ src/test/java/nemo/task/EventTest.java
@@ -0,0 +1,44 @@
+package nemo.task;  
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.HashMap;
+import java.text.ParseException;
+import java.time.format.DateTimeParseException;
+
+public class EventTest {
+    @Test
+    public void eventConstructorTest(){
+        HashMap<String, String> dummyArgs = new HashMap<>();
+        dummyArgs.put("mainArg", "test");
+        try {
+            new Event(dummyArgs);
+        } catch (ParseException e) {
+            assertEquals(e.getMessage(), "Task argument requires a from date");
+        }
+
+        dummyArgs.put("from", "a");
+        try {
+            new Event(dummyArgs);
+        } catch (ParseException e) {
+            assertEquals(e.getMessage(), "Task argument requires a to date");
+        }
+
+        dummyArgs.put("to", "b");
+        try {
+            new Event(dummyArgs);
+        } catch (DateTimeParseException e) {
+            assertEquals(e.getMessage(), "a is not a valid date for format dd-MM-yyyy HHmm.");
+        } catch (ParseException e) {
+
+        }
+
+        dummyArgs.put("from", "01-01-2026 0000");
+        dummyArgs.put("to", "01-01-2026 0000");
+        try {
+            new Event(dummyArgs);
+        } catch (Exception e) {
+        }
+    }
+}
