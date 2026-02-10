f58b7d52a3ddac9660b75b39221f89d84cddde2e
Sim Yue Yang <yueyangsim02@gamil.com>
2026-02-10 18:21:46 +0800

    Add assertions to validate correctness of Store
    Store class has methods relying on assumptions
    which are not being explicitly documented and
    checked
    
    Use assertions to explicitly state these
    assumptions.
    
    Let's use GPT-5 mini AI model to examine the code
    for where assertions are necessary to validate
    correctness of Store class
    
    Assertions are being added to the Store class to
    reduce the chances of these implicit assumptions
    being violated in a later commit

--- src/main/java/nemo/store/Store.java
+++ src/main/java/nemo/store/Store.java
@@ -21,10 +21,18 @@ public class Store {
     }
 
     public Task get(int id) {
+        assert this.tasks != null : "Store tasks list is null";
+        assert id >= 0 && id < this.tasks.size() : "Index out of bounds: " + id;
         return this.tasks.get(id);
     }
 
+    /**
+     * Add a new task to the tasks list
+     *
+     * @param task The new task to be added
+     */
     public void add(Task task) {
+        assert task != null : "task cannot be null";
         this.tasks.add(task);
     }
 
@@ -35,25 +43,47 @@ public class Store {
      *            Var args of tasks
      */
     public void add(Task... tasks) {
+        assert tasks != null : "tasks varargs cannot be null";
         for (int i = 0; i < tasks.length; i++) {
+            assert tasks[i] != null : "Task at index " + i + " is null";
             this.tasks.add(tasks[i]);
         }
     }
 
+    /**
+     * Remove a task at an index in the store tasks list
+     *
+     * @param i The index to remove the task from the list
+     */
     public void remove(int i) {
+        assert this.tasks != null : "Store tasks list is null";
+        assert i >= 0 && i < this.tasks.size() : "Index out of bounds: " + i;
         this.tasks.remove(i);
     }
 
+    /**
+     * Get the size of the store tasks list
+     *
+     * @return size of the store tasks list
+     */
     public int size() {
+        assert this.tasks != null : "Store tasks list is null";
         return this.tasks.size();
     }
 
+    /**
+     * Check if the store tasks list is empty or not
+     *
+     * @return boolean if the store tasks list is empty or not
+     */
     public boolean isEmpty() {
+        assert this.tasks != null : "Store tasks list is null";
         return this.tasks.isEmpty();
     }
 
     // Solution adapted from https://www.geeksforgeeks.org/java/iterators-in-java/
     public Iterator<Task> getIterator() {
+        assert this.tasks != null : "Store tasks list is null";
         return tasks.iterator();
     }
 
@@ -63,8 +93,13 @@ public class Store {
     * @param filteredPriority Priority to keep
     */
     public ArrayList<Task> filterByPriority(Priority filteredPriority) {
+        assert filteredPriority != null : "filteredPriority cannot be null";
+        assert this.tasks != null : "Store tasks list is null";
         return new ArrayList<>(tasks.stream()
-                .filter(t -> t.getPriority() == filteredPriority)
+                .filter(t -> {
+                    assert t != null : "task in store is null";
+                    return t.getPriority() == filteredPriority;
+                })
                 .toList());
     }
 
@@ -74,6 +109,8 @@ public class Store {
      * @param shouldSortHighestPriorityFirst Whether to sort highest priorities at the top
      */
     public void sortByPriority(boolean shouldSortHighestPriorityFirst) {
+        assert this.tasks != null : "Store tasks list is null";
+        assert tasks.stream().allMatch(t -> t != null && t.getPriority() != null) : "Found task with null priority";
         if (shouldSortHighestPriorityFirst) {
             tasks.sort(Comparator.comparing(Task::getPriority));
         } else {
@@ -87,6 +124,7 @@ public class Store {
      * @return list of task summaries
      */
     public String generateList() {
+        assert this.tasks != null : "Store tasks list is null";
         return generateListFormattedTasks(tasks);
     }
 
@@ -98,10 +136,12 @@ public class Store {
      * @return The list of tasks which matched the query
      */
     public ArrayList<Task> findTasks(String query) {
+        assert query != null : "query cannot be null";
         Iterator<Task> tasksIt = this.getIterator();
         ArrayList<Task> matchedTasks = new ArrayList<>();
         while (tasksIt.hasNext()) {
             Task task = tasksIt.next();
+            assert task != null : "task in store is null";
             if (task.getDescription().contains(query)) {
                 matchedTasks.add(task);
             }
@@ -117,9 +157,11 @@ public class Store {
      * @return list of task summaries
      */
     public static String generateListFormattedTasks(ArrayList<Task> tasksList) {
+        assert tasksList != null : "tasksList cannot be null";
         return IntStream.range(0, tasksList.size())
                 .mapToObj(i -> {
                     Task task = tasksList.get(i);
+                    assert task != null : "task at index " + i + " is null";
                     String threeDigitIndex = String.format("%03d", i + 1);
                     return MessageFormat.format("{0}.{1}\n", threeDigitIndex, task.getSummary());
                 })
