b3b5c5501663097e8b8ba2b46fda66caedc7327c
Sim Yue Yang <yueyangsim02@gamil.com>
2026-02-11 08:46:26 +0800

    Improve store priority filtering test

--- src/test/java/nemo/store/StoreTest.java
+++ src/test/java/nemo/store/StoreTest.java
@@ -2,14 +2,19 @@ package nemo.task;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.ArrayList;
+
 import org.junit.jupiter.api.Test;
 
 import nemo.store.Store;
 
 public class StoreTest {
     private static final String LOW_TEXT = "a";
+    private static final int LOW_COUNT = 1;
     private static final String MED_TEXT = "b";
+    private static final int MED_COUNT = 2;
     private static final String HIGH_TEXT = "c";
+    private static final int HIGH_COUNT = 3;
 
     @Test
     public void storeFilterByPriority_tasksVaryingPriority_taskOfSpecifiedPriorityReturned() {
@@ -21,12 +26,18 @@ public class StoreTest {
         store.add(new Todo(HIGH_TEXT, Priority.HIGH));
         store.add(new Todo(HIGH_TEXT, Priority.HIGH));
 
-        for (Task task : store.filterByPriority(Priority.LOW)) {
+        ArrayList<Task> lowPriorityTasks = store.filterByPriority(Priority.LOW);
+        assertEquals(lowPriorityTasks.size(), LOW_COUNT);
+        for (Task task : lowPriorityTasks) {
             assertEquals(task.getGoal(), LOW_TEXT);
         }
+        ArrayList<Task> medPriorityTasks = store.filterByPriority(Priority.MED);
+        assertEquals(medPriorityTasks.size(), MED_COUNT);
         for (Task task : store.filterByPriority(Priority.MED)) {
             assertEquals(task.getGoal(), MED_TEXT);
         }
+        ArrayList<Task> highPriorityTasks = store.filterByPriority(Priority.HIGH);
+        assertEquals(highPriorityTasks.size(), HIGH_COUNT);
         for (Task task : store.filterByPriority(Priority.HIGH)) {
             assertEquals(task.getGoal(), HIGH_TEXT);
         }
